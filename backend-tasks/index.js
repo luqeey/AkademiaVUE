fd9bd4e1293d05cbfcf976f236a0b26fc64b4a31
luqeey <115131091+luqeey@users.noreply.github.com>
2023-10-14 15:22:03 +0200

    prepajanie serveru mongoDb

--- backend-tasks/index.js
+++ backend-tasks/index.js
@@ -1,22 +1,43 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const multer = require('multer')
+
+const connection_string = "mongodb://localhost:27017/shoppinglist"
 
 const index = express()
-const port = 3000 
+// const port = 3000 
 
 index.use(bodyParser.json())
 
-const tasks = [
-    { id: 1, task: 'Task 1', description: 'Description 1' },
-    { id: 2, task: 'Task 2', description: 'Description 2' },
-]
+const database_name = "shoppinglist"
+var database;
 
-index.use(cors())
-index.get('/tasks', (req, res) => {
-    res.json(tasks)
+index.listen(5038, () => {
+    MongoClient.connect(connection_string, (error, client) => {
+        database = client.db(database_name)
+        console.log("mongo db connection succesful ")
+    })
 })
 
-index.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+index.get('/api/shoppinglist/GetNote', (request, response) => {
+    database.collection('shoppinglistcollection').find({}).toArray((error, result) => {
+        response.send(result)
+    })
 })
+
+
+// const tasks = [
+//     { id: 1, task: 'Task 1', description: 'Description 1' },
+//     { id: 2, task: 'Task 2', description: 'Description 2' },
+// ]
+
+// index.use(cors())
+// index.get('/tasks', (req, res) => {
+//     res.json(tasks)
+// })
+
+// index.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// })
